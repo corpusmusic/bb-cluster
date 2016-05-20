76660ddcac5ca721cc20f80d398428729382842f
CalvineHicks <calvinehicks@gmail.com>
2016-05-20 11:20:36 -0600

    kMeans cluster implemented

--- kmean_cluster.py
+++ kmean_cluster.py
@@ -1,15 +1,34 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 import csv
 import numpy as np
+import os
 import scipy as sp
 from sklearn.cluster import KMeans
 
 #open transition prob csv file
-csvf = open('songbysongtransprob.csv','rb')
-reader = csv.reader(csvf)
-for row in reader:
-    print row
+#with open('songbysongtransprob.csv','rb') as csvf:
+#    rows = csv.reader(csvf)
+#    for row in rows:
+#        print row
+        
+def read_data(filename):
+    with open(filename, 'rB') as csvf:
+        return [row for row in csv.reader(csvf)]
 
+X = read_data("songbysongtransprob.csv")
+Y = []
+for song in X:
+    Y.append(song[0])
+    song.pop(0)
+    
 K = 15 #number of clusters
 km = KMeans(n_clusters = K)
-km.fit(customer_deal_matrix)
+km.fit(X,Y)
+#print(km.labels_)
+#print(km.cluster_centers_[1])
+with open('song_cluster_kMeans.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=str(u','), quotechar=str(u'"'), quoting=csv.QUOTE_MINIMAL)
+    	
+    for title, cluster in zip(Y, km.labels_):
+        writer.writerow([title, cluster])
+    writer.writerow([])
