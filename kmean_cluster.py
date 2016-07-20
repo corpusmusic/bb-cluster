aed223ddc9432e06f697acb36467d73b73cf119a
Kris Shaffer <kris.shaffer@gmail.com>
2016-07-20 10:32:06 -0400

    kmean-cluster.py now runs a full battery

--- kmean_cluster.py
+++ kmean_cluster.py
@@ -5,76 +5,66 @@ import os
 import scipy as sp
 from sklearn.cluster import KMeans
 
+# general parameters for project
+
+maxClusters = 15 # algorithm will run from 1 cluster to this cardinality
+epochsPerCardinality = 10 # number of epochs to run for each cardinality
+outputFolder = 'cluster_output_data/' # where the output data will be stored
+inertiaFile = 'inertia.csv' # where the inertia for each epoch is recorded
+
+# custom functions needed for project
+
 def read_data(filename):
-    with open(filename, 'rB') as csvf:
+    with open(filename, 'r') as csvf:
         return [row for row in csv.reader(csvf)]
 
 def get_cluster(title):
     return clusterIdOfSong[title]
 
-X = read_data("songbysongtransprob.csv")
-Y = []
+# create the array and song list needed for KMeans.fit
+
+X = read_data("songbysongtransprob.csv") # array of first-order conditional probabilities of chord occurrence
+Y = [] # list of song titles for indexing
 for song in X:
     Y.append(song[0])
     song.pop(0)
 
+inertiaData = {}
 
-"""15 Cluster K-Means Analysis"""
-K = 15 #number of clusters
-km = KMeans(n_clusters = K, n_init = 10000)
-km.fit(X,Y)
+# Run the cluster analysis (km.fit)
 
-clusterIdOfSong = {}
-for title, cluster in zip(Y, km.labels_):
-    clusterIdOfSong[title] = cluster
+K = 1 # number of clusters
+while K <= maxClusters:
+    epoch = 1 # iteration of cluster algorithm
+    while epoch <= epochsPerCardinality:
+        km = KMeans(n_clusters = K, max_iter = 10000, n_init = 10000) # define the algorithm parameters
+        km.fit(X,Y) # run cluster algorithm
+        epochName = 'cluster' + str(K) + '_epoch' + str(epoch)
+        inertiaData[epochName] = km.inertia_ # addd within-cluster sum-of-squares for the solution to dictionary
 
-"""Write temp file outputs"""
+        # assemble a dictionary containing the cluster membership
+        # for each song in the corpus
+        # write to file
 
-csvfile = 'chord_by_chord.csv'
-with open(csvfile, 'rb') as fin, open('cluster_'+csvfile, 'wb') as fout:                #temp file for k = 6
-    reader = csv.reader(fin, lineterminator='\n')
+        clusterIdOfSong = {}
+        for title, cluster in zip(Y, km.labels_):
+            clusterIdOfSong[title] = cluster
+
+        csvfile = outputFolder + epochName + '.csv' # filename identifies the number of clusters and which epoch
+        with open(csvfile, 'w') as fout:
+            writer = csv.writer(fout, lineterminator='\n')
+            for title in clusterIdOfSong:
+                writer.writerow((title, clusterIdOfSong[title]))
+
+    # reset for next loop
+        print(epochName, 'completed.')
+        epoch += 1
+    K += 1
+
+# write inertia data to file
+
+csvfile = outputFolder + inertiaFile
+with open(csvfile, 'w') as fout:
     writer = csv.writer(fout, lineterminator='\n')
-    for song in reader:
-        if len(song) > 0:
-            song.append(get_cluster(song[0]))
-            writer.writerow(song)
-
-csvfile2 = 'songbysongtransprob.csv'
-with open(csvfile2, 'rb') as fin, open('cluster_'+csvfile2, 'wb') as fout:              #temp file for k = 6
-    reader = csv.reader(fin, lineterminator='\n')
-    writer = csv.writer(fout, lineterminator='\n')
-    for song in reader:
-        if len(song) > 0:
-            song.append(get_cluster(song[0]))
-            writer.writerow(song)
-
-
-"""6 Cluster K-Means Analysis"""
-K = 6 #number of clusters
-km = KMeans(n_clusters = K, n_init = 10000)
-km.fit(X,Y)
-
-clusterIdOfSong = {}
-for title, cluster in zip(Y, km.labels_):
-    clusterIdOfSong[title] = cluster
-
-
-"""Write final file outputs with K=6 and K=15 cluster output appends"""
-
-csvfile = 'cluster_chord_by_chord.csv'
-with open(csvfile, 'rb') as fin, open('new_'+csvfile, 'wb') as fout:
-    reader = csv.reader(fin, lineterminator='\n')
-    writer = csv.writer(fout, lineterminator='\n')
-    for song in reader:
-        if len(song) > 0:
-            song.append(get_cluster(song[0]))
-            writer.writerow(song)
-
-csvfile2 = 'cluster_songbysongtransprob.csv'
-with open(csvfile2, 'rb') as fin, open('new_'+csvfile2, 'wb') as fout:
-    reader = csv.reader(fin, lineterminator='\n')
-    writer = csv.writer(fout, lineterminator='\n')
-    for song in reader:
-        if len(song) > 0:
-            song.append(get_cluster(song[0]))
-            writer.writerow(song)
+    for epochName in inertiaData:
+        writer.writerow((epochName, inertiaData[epochName]))
