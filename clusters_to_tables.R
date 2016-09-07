5e75c9baa4d9751e8cf1339a6ff4bd9811d966e4
DTLT <dtlt@ITCC-410-VID.local>
2016-09-07 13:30:51 -0400

    CSV transitional probability tables created for all clusters and epochs!

--- clusters_to_tables.R
+++ clusters_to_tables.R
@@ -1,14 +1,23 @@
 source_folder <- 'cluster_tables/'
 output_folder <- 'cluster_summaries/'
 filename <- 'cluster6_1.csv'
-d <- read.csv(paste(source_folder, filename, sep=''))
-summary(d)
-cm <- colMeans(d[c(2:145)])
-cmtable <- data.frame()
-cmtable <- rbind(cm[c(1:12)], cm[c(13:24)], cm[c(25:36)], cm[c(37:48)], cm[c(49:60)], cm[c(61:72)], cm[c(73:84)], cm[c(85:96)], cm[c(97:108)], cm[c(109:120)], cm[c(121:132)], cm[c(133:144)])
-rn <- c('I', 'bII', 'II', 'bIII', 'III', 'IV', 'bV', 'V', 'bVI', 'VI', 'bVII', 'VII')
-rownames(cmtable) <- rn
-colnames(cmtable) <- rn
-summary(cmtable)
-cmtable
-write.csv(cmtable, file = paste(output_folder, filename, sep=''))
+cluster_files <- list()
+
+for (i in 1:15) {
+  for (j in 1:i) {
+    cluster_files <- c(cluster_files, paste('cluster', i, '_', j, '.csv', sep = ''))
+  }
+}
+
+create_mean_table <- function(filename) {
+  d <- read.csv(paste(source_folder, filename, sep=''))
+  cm <- colMeans(d[c(2:145)])
+  cmtable <- data.frame()
+  cmtable <- rbind(cm[c(1:12)], cm[c(13:24)], cm[c(25:36)], cm[c(37:48)], cm[c(49:60)], cm[c(61:72)], cm[c(73:84)], cm[c(85:96)], cm[c(97:108)], cm[c(109:120)], cm[c(121:132)], cm[c(133:144)])
+  rn <- c('I', 'bII', 'II', 'bIII', 'III', 'IV', 'bV', 'V', 'bVI', 'VI', 'bVII', 'VII')
+  rownames(cmtable) <- rn
+  colnames(cmtable) <- rn
+  write.csv(cmtable, file = paste(output_folder, filename, sep=''))
+}
+
+lapply(cluster_files, create_mean_table)
