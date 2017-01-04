9028c2689fe8e2a0dd41a5faef71ffd8533e2b76
Kris Shaffer <kris.shaffer@gmail.com>
2017-01-04 12:49:49 -0500

    viz images

--- visualizations.R
+++ visualizations.R
@@ -1,9 +1,5 @@
 library('ggplot2')
-c1 <- read.csv('cluster_summaries/cluster1_1.csv')
-
 library(reshape2)
-c <- melt(c1, id.var="X")
-colnames(c) <- c('start', 'arrival', 'rawProbability')
 
 normalizeProb <- function(record)  {
   prob_sum <- sum(c[c$start==record['start'],]['rawProbability'])
@@ -11,15 +7,40 @@ normalizeProb <- function(record)  {
   return(norm_sum)
 }
 
-probability <- apply(c, MARGIN = 1, normalizeProb)
-c_normalized <- cbind(c, probability)
-c_normalized$start <- factor(c_normalized$start, levels = 
-              c('I', 'bII', 'II', 'bIII', 'III', 'IV', 'bV', 'V', 'bVI', 'VI', 'bVII', 'VII'))
+build_file_name <- function(number, solution) {
+  return(paste('cluster', solution, '_', number, sep = ''))
+}
 
-ggplot(data = c_normalized, mapping = aes(x = start, y = probability, fill = arrival)) +
-  geom_bar(stat = "identity") +
-  xlab('Starting chord') +
-  ylab('Probability') +
-  ggtitle('Transitional probabilities') +
-  guides(fill=guide_legend(title="Arrival chord")) +
-  theme(plot.title = element_text(hjust = 0.5))
+generate_file_name_list <- function(cardinality) {
+  name_list <- sapply(1:cardinality, build_file_name, cardinality)
+}
+
+savePlotPNG <- function(solution) {
+  c1 <- read.csv(paste('cluster_summaries/', solution, '.csv', sep=''))
+  c <- melt(c1, id.var="X")
+  colnames(c) <- c('start', 'arrival', 'rawProbability')
+  
+  probability <- apply(c, MARGIN = 1, normalizeProb)
+  c_normalized <- cbind(c, probability)
+  c_normalized$start <- factor(c_normalized$start, levels = 
+                                 c('I', 'bII', 'II', 'bIII', 'III', 'IV', 'bV', 'V', 'bVI', 'VI', 'bVII', 'VII'))
+  
+  png(filename = paste('plots/', solution, '.png', sep=''), width=1200, height=500)
+  
+  g <- ggplot(data = c_normalized, mapping = aes(x = start, y = probability, fill = arrival)) +
+    geom_bar(stat = "identity") +
+    xlab('Starting chord') +
+    ylab('Probability') +
+    ggtitle('Transitional probabilities') +
+    guides(fill=guide_legend(title="Arrival chord")) +
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  print(g)
+  dev.off()
+}
+
+solutions <- unlist(sapply(1:15, generate_file_name_list))
+
+sapply(solutions, savePlotPNG)
+
+savePlotPNG('cluster1_1')
